08b48484c858bdad7e9fa8d76e71cd4b28090eba
Jukeri <jukeri12@protonmail.com>
2020-04-03 14:57:25 +0300

    Initial commit

---
+++ functions.php
@@ -0,0 +1,11 @@
+<?php
+add_action( 'wp_enqueue_scripts', 'parkour_festivaali_enqueue_styles' );
+function parkour_festivaali_enqueue_styles() {
+
+	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+	wp_enqueue_style( 'child-style',
+		get_stylesheet_directory_uri() . '/style.css',
+		array( 'parent-style' ),
+		wp_get_theme()->get('Version')
+	);
+}
