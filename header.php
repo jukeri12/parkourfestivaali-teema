b7227e4e26c1acdc561a7176cd19c7a0e426bdbf
Jukeri <jukeri12@protonmail.com>
2020-05-14 20:01:12 +0300

    more missing modifications... thanks eclipse

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
  * Header file for the Sivistymättömät Festival theme.
  * Modified to add Hero Carousel to front page, changes mobile menu icon
  * 
- * Sivistymattomat Festival theme, Copyright 2020 Sivistymättömät workgroup
+ * Sivistymattomat Festival theme modifications, Copyright 2020 Sivistymättömät workgroup
  * Based on the Twenty Twenty WordPress default theme, Copyright 2019-2020 WordPress.org
  * Licensed as GPL 2.0 or later
  *
--- template-parts/footer-menus-widgets.php
+++ template-parts/footer-menus-widgets.php
@@ -6,7 +6,7 @@
  * Modified to add a third sidebar so we can have a 3-bar layout (for important links)
  * 
  * 
- * Sivistymattomat Festival theme, Copyright 2020 Sivistymättömät workgroup
+ * Sivistymattomat Festival theme modifications, Copyright 2020 Sivistymättömät workgroup
  * Based on the Twenty Twenty WordPress default theme, Copyright 2019-2020 WordPress.org
  * Licensed as GPL 2.0 or later
  *
